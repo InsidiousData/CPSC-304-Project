307786858530fb0417a4addedb090300dcad2db0
kelvincao <kelvincao6@gmail.com>
2021-06-10 15:00:16 -0700

    initial codebase commit

---
+++ main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Basketball Repository</h1>
+<?php
+include 'connect.php';
+$conn = OpenCon();
+echo "Connected Successfully";
+$sql = "SELECT BT_Name, BT_Total_wins, BT_Total_losses, BT_Total_games_played FROM basketballteam";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		echo "Name: " . $row["BT_Name"]. "<br>";
+	}
+} else {
+	echo "0 results";
+}
+CloseCon($conn);
+?>
+</body>
+</html>
